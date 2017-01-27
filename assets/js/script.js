a1dbf6b7dd2145b8ef0c897237a7661a56b84fcd
Roman Levytskyy <r.levyts@gmail.com>
2017-01-26 20:46:09 -0500

    fix safari, ipad and iphone bug where active section in the navbar wouldn't change text color

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,17 @@ $(document).ready(function(){
   });
 
   var colors = {
-    'about': 'rgb(142,85,114)',
-    'current': '#F46036',
-    'finished': '#2E294E'
-  }
-  var address = window.location.href.split('/'),
-      pagename = address[address.length - 1],
-      navtag = pagename.split('_')[0];
+        'about': 'rgb(142,85,114)',
+        'current': '#F46036',
+        'finished': '#2E294E'
+      },
+      address = window.location.href.split('/'),
+      pagename = address[address.length - 1]
 
   $('body').css('background-color', colors[pagename]);
   $('.card-action a').css('color', colors[pagename]);
-  $('nav a').attr('style', 'color: ' + colors[pagename] + ' !important;');
-  $("a:contains('" + navtag + "')").parent().css('background-color', colors[pagename]);
-  $("a:contains('" + navtag + "')").css('color', 'white');
+  $('nav a').attr('style', 'color: ' + colors[pagename] + '');
+  $("a:contains('" + pagename + "')").parent().css('background-color', colors[pagename]);
+
+  $("a:contains('" + pagename + "')").css('color', "white");
 });
