d2c1c6a6c7f713a8ff90f290c29e8d6aec7d52c5
levytsroman <r.levyts@gmail.com>
2017-01-27 15:00:35 -0500

    add feature making projects filterable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  $('select').material_select();
+
   $(".button-collapse").sideNav({
     menuWidth: 200,
     edge: 'left',
@@ -20,4 +22,22 @@ $(document).ready(function(){
   $("a:contains('" + pagename + "')").parent().css('background-color', colors[pagename]);
 
   $("a:contains('" + pagename + "')").css('color', "white");
+
+  $('select').change(function(event){
+    debugger
+    var tag = $(this).val(),
+        projects = $('.card');
+
+        for(var i=0; i < projects.length; i++){
+          console.log("I is :" + i);
+          var $project = $(projects[i]);
+          // debugger
+          if($project.attr('tags').indexOf(tag) > -1 || tag === "all"){
+            // debugger
+            $project.show();
+          } else {
+            $project.hide();
+          }
+        }
+  })
 });
