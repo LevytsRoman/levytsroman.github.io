a1a4862caf4fd8d893c8557b64fbf67636f8ae92
levytsroman <r.levyts@gmail.com>
2017-01-30 17:48:22 -0500

    remove debugging statements, optimiza the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,21 +40,20 @@ $(document).ready(function(){
       $button.html(tag + '<i class="material-icons left">close</i>');
       $button.addClass("active_tag");
     }
+
     for(var j=0; j < buttons.length; j++){
-      // console.log("hello" + $(buttons[j]).attr('class').indexOf('active_tag'))
-      if($(buttons[j]).attr('class').indexOf('active_tag') > -1){
-        // debugger
-        active_tags.push($(buttons[j]).attr('val'));
+      $current_button = $(buttons[j]);
+      if($current_button.attr('class').indexOf('active_tag') > -1){
+        active_tags.push($current_button.attr('val'));
       }
     }
-
-    // debugger
 
     for(var i=0; i < projects.length; i++){
       var $project = $(projects[i]);
       $project.hide();
       for(var k=0; k < active_tags.length; k++){
-        if($project.attr('tags').indexOf(active_tags[k]) > -1 || active_tags[k] === "all" || active_tags[k] === ""){
+        var active_tag = active_tags[k];
+        if($project.attr('tags').indexOf(active_tag) > -1 || active_tag === "all" || active_tag === ""){
           $project.show();
         }
       }
