d93c6dd2c4d83343d98bd24f68d394f4b9571c3b
levytsroman <r.levyts@gmail.com>
2017-01-31 18:05:43 -0500

    fix navbar not rendering navigation links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,3 @@
-function clearFilters(){
-  $(".closing-icon").remove();
-  $(".active_tag").removeClass('active_tag');
-  $('.card').show();
-}
-
-function toggleTags(button, tag){
-  if(button.attr('class').indexOf('active_tag') > -1){
-    button.removeClass("active_tag");
-    button.html(tag);
-  } else {
-    button.html(tag + '<i class="material-icons left closing-icon">close</i>');
-    button.addClass("active_tag");
-  }
-}
-
-function addActiveTags(buttons){
-  var active_tags = [];
-  for(var j=0; j < buttons.length; j++){
-    current_button = $(buttons[j]);
-    if(current_button.attr('class').indexOf('active_tag') > -1){
-      active_tags.push(current_button.attr('val'));
-    }
-  }
-  return active_tags;
-}
-
-function showProjects(projects, active_tags){
-  projects.each(function(i){
-    var project = $(projects[i]);
-    project.hide();
-
-    $.each(active_tags, function(k){
-      var active_tag = active_tags[k],
-      projectContainsTag = project.attr('tags').indexOf(active_tag) > -1;
-
-      if( projectContainsTag){
-        project.show();
-      }
-    })
-  })
-  
-  var activeTagsAreEmpty = active_tags.length === 0;
-
-  if(activeTagsAreEmpty){
-    projects.show();
-  }
-}
-
 $(document).ready(function(){
   $(".button-collapse").sideNav({
     menuWidth: 200,
@@ -90,3 +41,52 @@ $(document).ready(function(){
     }
   })
 });
+
+function clearFilters(){
+  $(".closing-icon").remove();
+  $(".active_tag").removeClass('active_tag');
+  $('.card').show();
+}
+
+function toggleTags(button, tag){
+  if(button.attr('class').indexOf('active_tag') > -1){
+    button.removeClass("active_tag");
+    button.html(tag);
+  } else {
+    button.html(tag + '<i class="material-icons left closing-icon">close</i>');
+    button.addClass("active_tag");
+  }
+}
+
+function addActiveTags(buttons){
+  var active_tags = [];
+  for(var j=0; j < buttons.length; j++){
+    current_button = $(buttons[j]);
+    if(current_button.attr('class').indexOf('active_tag') > -1){
+      active_tags.push(current_button.attr('val'));
+    }
+  }
+  return active_tags;
+}
+
+function showProjects(projects, active_tags){
+  projects.each(function(i){
+    var project = $(projects[i]);
+    project.hide();
+
+    $.each(active_tags, function(k){
+      var active_tag = active_tags[k],
+      projectContainsTag = project.attr('tags').indexOf(active_tag) > -1;
+
+      if( projectContainsTag){
+        project.show();
+      }
+    })
+  })
+
+  var activeTagsAreEmpty = active_tags.length === 0;
+
+  if(activeTagsAreEmpty){
+    projects.show();
+  }
+}
