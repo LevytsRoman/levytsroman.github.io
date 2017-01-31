8a4ae5a39a706a91ff2f9ca9961f96a575ce3005
levytsroman <r.levyts@gmail.com>
2017-01-31 15:26:29 -0500

    get rid of global variables, use jQuery each instead of for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,40 +23,47 @@ $(document).ready(function(){
 
   $("a:contains('" + pagename + "')").css('color', "white");
 
+  function clearFilters(){
+    // remove all close icons
+    // render all cards
+  }
+
   $('.tag').click(function(event){
     event.preventDefault();
+
     $(this).children().remove('i');
 
-    var $button = $(this),
+    var button = $(this),
         tag = $(this).attr('val'),
         projects = $('.card'),
         buttons = $('.btn'),
         active_tags = [];
 
-    if($button.attr('class').indexOf('active_tag') > -1){
-      $button.removeClass("active_tag");
-      $button.html(tag);
+    if(button.attr('class').indexOf('active_tag') > -1){
+      button.removeClass("active_tag");
+      button.html(tag);
     } else {
-      $button.html(tag + '<i class="material-icons left">close</i>');
-      $button.addClass("active_tag");
+      button.html(tag + '<i class="material-icons left">close</i>');
+      button.addClass("active_tag");
     }
 
     for(var j=0; j < buttons.length; j++){
-      $current_button = $(buttons[j]);
-      if($current_button.attr('class').indexOf('active_tag') > -1){
+      current_button = $(buttons[j]);
+      if(current_button.attr('class').indexOf('active_tag') > -1){
         active_tags.push($current_button.attr('val'));
       }
     }
 
-    for(var i=0; i < projects.length; i++){
-      var $project = $(projects[i]);
-      $project.hide();
+    projects.each(function(i){
+      var project = $(projects[i]);
+      project.hide();
+
       for(var k=0; k < active_tags.length; k++){
         var active_tag = active_tags[k];
-        if($project.attr('tags').indexOf(active_tag) > -1 || active_tag === "all" || active_tag === ""){
-          $project.show();
+        if(project.attr('tags').indexOf(active_tag) > -1 || active_tags == []){
+          project.show();
         }
       }
-    }
+    })
   })
 });
