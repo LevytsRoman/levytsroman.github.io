8fb7f54225be49b7a2ba6cd0e349675f72a45289
Roman Levytskyy <r.levyts@gmail.com>
2017-10-11 20:47:18 -0400

    play with barba js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,10 @@ function assignColors(pagename, color){
   $("a:contains('" + pagename + "')").css('color', "white");
 }
 
-$(document).ready(function(){
+function initializeScripts(){
+
+
+
   $(".button-collapse").sideNav({
     menuWidth: 200,
     edge: 'left',
@@ -81,6 +84,18 @@ $(document).ready(function(){
       pagename = key;
     }
   });
+  Barba.Pjax.getTransition = function() {
+    return slideOutTransition;
+  };
+  
+  // if(urlString === 'http://localhost:4000/projects'){
+  //
+  // } else {
+  //   Barba.Pjax.getTransition = function() {
+  //     return FadeTransition;
+  //   };
+  // }
+
 
   assignColors(pagename, color);
 
@@ -132,4 +147,127 @@ $(document).ready(function(){
     $("html, body").animate({ scrollTop: 0 }, 250);
     return false;
   })
+}
+
+$(document).ready(function(){
+  Barba.Pjax.start();
+  initializeScripts();
 });
+
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
+
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
+});
+
+var slideOutTransition = Barba.BaseTransition.extend({
+  tart: function() {
+    Promise
+      .all([this.newContainerLoading, this.fadeOut()])
+      .then(this.fadeIn.bind(this));
+  },
+
+  fadeOut: function() {
+    /**
+     * this.oldContainer is the HTMLElement of the old Container
+     */
+    return $(this.oldContainer).animate({ opacity: 0 }).promise();
+  },
+
+  fadeIn: function() {
+    /**
+     * this.newContainer is the HTMLElement of the new Container
+     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
+     * Please note, newContainer is available just after newContainerLoading is resolved!
+     */
+
+    var _this = this;
+    var $el = $(this.newContainer);
+
+    $(this.oldContainer).hide();
+
+    $el.css({
+      visibility : 'visible',
+      opacity : 0
+    });
+
+    $el.animate({ opacity: 1 }, 400, function() {
+      /**
+       * Do not forget to call .done() as soon your transition is finished!
+       * .done() will automatically remove from the DOM the old Container
+       */
+
+      _this.done();
+    });
+    initializeScripts()
+  }
+})
+
+var FadeTransition = Barba.BaseTransition.extend({
+  start: function() {
+    /**
+     * This function is automatically called as soon the Transition starts
+     * this.newContainerLoading is a Promise for the loading of the new container
+     * (Barba.js also comes with an handy Promise polyfill!)
+     */
+
+    // As soon the loading is finished and the old page is faded out, let's fade the new page
+    Promise
+      .all([this.newContainerLoading, this.fadeOut()])
+      .then(this.fadeIn.bind(this));
+  },
+
+  fadeOut: function() {
+    /**
+     * this.oldContainer is the HTMLElement of the old Container
+     */
+
+    return $(this.oldContainer).animate({ opacity: 0 }).promise();
+  },
+
+  fadeIn: function() {
+    /**
+     * this.newContainer is the HTMLElement of the new Container
+     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
+     * Please note, newContainer is available just after newContainerLoading is resolved!
+     */
+
+    var _this = this;
+    var $el = $(this.newContainer);
+
+    $(this.oldContainer).hide();
+
+    $el.css({
+      visibility : 'visible',
+      opacity : 0
+    });
+
+    $el.animate({ opacity: 1 }, 400, function() {
+      /**
+       * Do not forget to call .done() as soon your transition is finished!
+       * .done() will automatically remove from the DOM the old Container
+       */
+
+      _this.done();
+    });
+    initializeScripts()
+  }
+});
+
+/**
+ * Next step, you have to tell Barba to use the new Transition
+ */
+
+Barba.Pjax.getTransition = function() {
+  /**
+   * Here you can use your own logic!
+   * For example you can use different Transition based on the current page or link...
+   */
+
+  return FadeTransition;
+};
